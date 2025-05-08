555ba7b332099a591b53526fc79f12c9ead09fae
pisethmararika <mararyka.piseth@institute.pse.ngo>
2025-05-08 14:07:04 +0700

    update-menu

--- src/app/components/layoutWrapper.tsx
+++ src/app/components/layoutWrapper.tsx
@@ -3,16 +3,8 @@
 import { usePathname } from "next/navigation";
 import Navbar from "./navbar";
 import { SidebarItem } from "./sidebarItem";
-import {
-  Package,
-  Store,
-  PackageX,
-  LayoutDashboard,
-  BoxIcon,
-  Boxes,
-  User,
-} from "lucide-react";
-import { Package, Store, PackageX, Layers } from "lucide-react";
+
+import { Package, Store, PackageX, Layers, LayoutDashboard, BoxIcon, Boxes, User } from "lucide-react";
 
 export default function LayoutWrapper({
   children,
@@ -29,7 +21,7 @@ export default function LayoutWrapper({
   return (
     <>
       <Navbar />
-      <div className="flex h-[850px]">
+      <div className="flex h-[750px]">
         {/* Sidebar */}
         <div className="w-90 bg-[#2D579A] text-white text-[20px]">
           <nav className="mt-6">
@@ -40,44 +32,7 @@ export default function LayoutWrapper({
             <SidebarItem icon={<Package size={25} />} text="Stock In" href="/stockin" />
             <SidebarItem icon={<PackageX size={25} />} text="Stock Out" href="/stockout" />
             <SidebarItem icon={<User size={25} />} text="Create User" href="/register" />
-            <SidebarItem
-              icon={<Package size={25} />}
-              text="Dashboard"
-              href="/dashboard"
-            />
-            <SidebarItem
-              icon={<Store size={25} />}
-              text="Suppliers"
-              href="/suppliers"
-            />
-            <SidebarItem
-              icon={<Layers size={25} />}
-              text="Category"
-              href="/category"
-            />
-            <SidebarItem
-              icon={<Package size={25} />}
-              text="Product"
-              href="/product"
-            />
-            <SidebarItem
-              icon={<Package size={25} />}
-              text="Stock In"
-              href="/stockin"
-            />
-
-           
-            <SidebarItem
-              icon={<PackageX size={25} />}
-              text="Stock Out"
-              href="/stock-out"
-            />
-
-            <SidebarItem
-              icon={<Package size={25} />}
-              text="Create User"
-              href="/register"
-            />
+            
           </nav>
         </div>
 
